103afb28fedac58385e4541e21334d1dfaf97907
nicolesa <nsantarsiero@collingmedia.com>
2025-06-08 15:09:34 -0700

    fixing mroe build errors

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,10 @@
 const nextConfig = {
     output: 'export',
     images: {
-      unoptimized: true,
+      unoptimized: true, // needed for GitHub Pages
     },
-    basePath: '/portfolio-site-next', // change this to your repo name
+    basePath: "/portfolio-site-next", 
+    assetPrefix: "/portfolio-site-next",
   };
   
   export default nextConfig;
