27bbe23f916b3e14b214e268f526c293eb750cb0
nicolesa <nsantarsiero@collingmedia.com>
2025-06-08 15:39:38 -0700

    proper build

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    output: 'export', // Required for static hosting like GitHub Pages or Netlify
     images: {
-      unoptimized: true, // needed for GitHub Pages
+      unoptimized: true, // Needed since Next.js image optimization doesn't work without a server
     },
-    basePath: "/portfolio-site-next", 
-    assetPrefix: "/portfolio-site-next",
   };
   
   export default nextConfig;
