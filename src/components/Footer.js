bd1f5282c10b924bcfc9ed7799598d28407118db
nicolesa <nsantarsiero@collingmedia.com>
2025-06-08 15:05:43 -0700

    fixing build errors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,15 @@
 // components/Footer.js
+import Link from 'next/link';
+
 export default function Footer() {
     return (
       <footer className="site-footer">
         <div className="footer-content">
           <p>&copy; 2025 Nicole Santarsiero. All rights reserved.</p>
           <div className="footer-links">
-            <a href="/">Home</a> | 
-            <a href="https://www.linkedin.com/in/nicole-santarsiero-81443752" target="_blank" rel="noopener noreferrer">LinkedIn</a> | 
-            <a href="/contact">Contact</a>
+            <Link href="/">Home</Link> | 
+            <Link href="https://www.linkedin.com/in/nicole-santarsiero-81443752" target="_blank" rel="noopener noreferrer">LinkedIn</Link> | 
+            <Link href="/contact">Contact</Link>
           </div>
         </div>
       </footer>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,33 @@
 // components/Navbar.js
+import Link from 'next/link';
+
 export default function Navbar() {
   return (
     <header className="frame">
       <div className="nav-links">
-        <a className="scroll-link" href="/#about-me">
+        <Link className="scroll-link" href="/#about-me">
           About Me
-        </a>
-        <a className="scroll-link" href="/#my-work">
+        </Link>
+        <Link className="scroll-link" href="/#my-work">
           My Work
-        </a>
-        <a href="/contact">Contact</a>
+        </Link>
+        <Link href="/contact">Contact</Link>
       </div>
       <div className="ns-logo">
-        <a href="/">
+        <Link href="/">
           <img
             className="logo"
             src="/img/ns_white.png"
             alt="Nicole Santarsiero logo"
           />
-        </a>
-        <a
+        </Link>
+        <Link
           href="https://www.linkedin.com/in/nicole-santarsiero-81443752"
           target="_blank"
           rel="noopener noreferrer"
         >
           <i className="devicon-linkedin-plain colored"></i>
-        </a>
+        </Link>
       </div>
     </header>
   );
