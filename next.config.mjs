cf947184542266fe1086aaa3c25e8ec26f7485b5
nicolesa <nsantarsiero@collingmedia.com>
2025-06-08 15:00:25 -0700

    setting up build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true,
+    },
+    basePath: '/portfolio-site-next', // change this to your repo name
+  };
+  
+  export default nextConfig;
+  
