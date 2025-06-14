8a807e9132049e9696f947a417994c34e5016b3c
nicolesa <nsantarsiero@collingmedia.com>
2025-06-13 20:21:25 -0700

    adding build docs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'export', // Use static export mode
+  distDir: 'docs',  // Output to /docs instead of .next
   images: {
-    unoptimized: true,
+    unoptimized: true, // Required for static export if using <Image>
   },
 };
 
