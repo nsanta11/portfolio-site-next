34770681c00f03d6f84b67c75e643400d768942d
Nicole Santarsiero <nicolesa@rr4092.local>
2026-06-16 12:45:31 -0700

    new out folder

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Use static export mode
-  distDir: 'docs',  // Output to /docs instead of .next
+  distDir: 'out',  // Output to /docs instead of .next
   images: {
     unoptimized: true, // Required for static export if using <Image>
   },
